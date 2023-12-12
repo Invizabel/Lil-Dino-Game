abf2432a066aba8a7edd3b314e61abdf4645c369
Invizabel <46697708+Invizabel@users.noreply.github.com>
2023-12-12 16:58:51 -0600

    Add files via upload

--- Lil Dino Game/Python/main.py
+++ Lil Dino Game/Python/main.py
@@ -21,26 +21,12 @@ def main():
             new_map_data.append(1)
         map_data.append(new_map_data)
 
-    dino_data = []
-    spawned_bool = True
-    for x in range(12):
-        new_dino_data = []
-        for y in range(12):
-            if spawned_bool:
-                spawned_bool = False
-                new_dino_data.append(1)
-            else:
-                new_dino_data.append(0)
-        dino_data.append(new_dino_data)
-    
+    dino_x = 0
+    dino_y = 0
+    dino_data = [dino_x,dino_y]
+
     while True:
         display.fill((0,0,0))
-        for y, row in enumerate(map_data):
-            for x, tile in enumerate(row):
-                if tile:
-                    display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5))
-
-        screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -51,13 +37,39 @@ def main():
                     pygame.quit()
                     sys.exit()
 
-        for y, row in enumerate(dino_data):
+        # spawn terrain
+        for y, row in enumerate(map_data):
             for x, tile in enumerate(row):
                 if tile:
-                    display.blit(triceratops_img, (150 + x * 10 - y * 10  + (grass_img.get_width() - triceratops_img.get_width()) // 2, 100 + x * 5 + y * 5 - triceratops_img.get_height() + 15))
+                    display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5))
 
         screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
 
+        # generate movement
+        direction = random.randint(1,4)
+
+        if direction == 1 and dino_data[0] > 0:
+            dino_data[0] -= 1
+
+        if direction == 2 and dino_data[0] < 15:
+            dino_data[0] += 1
+
+        if direction == 3 and dino_data[0] > 0:
+            dino_data[1] -= 1
+
+        if direction == 4 and dino_data[0] < 15:
+            dino_data[1] += 1
+                
+        
+        # spawn dinosaurs
+        for y, row in enumerate(map_data):
+            for x, tile in enumerate(row):
+                if [x,y] == dino_data:
+                    display.blit(triceratops_img, (150 + x * 10 - y * 10  + (grass_img.get_width() - triceratops_img.get_width()) // 2, 100 + x * 5 + y * 5 - triceratops_img.get_height() + 15))
+                    
+        screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
+
         pygame.display.update()
+        time.sleep(1)
 
 main()
